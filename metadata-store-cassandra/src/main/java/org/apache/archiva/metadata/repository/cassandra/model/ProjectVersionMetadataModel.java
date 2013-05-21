2850f0eeec42e5aa950f3d60033f8c1d47e4ef28
Olivier Lamy <olamy@apache.org>
2013-05-21 13:28:54 +0000

    license header
    git-svn-id: https://svn.apache.org/repos/asf/archiva/sandbox/trunk@1484802 13f79535-47bb-0310-9956-ffa450edef68

--- metadata-store-cassandra/src/main/java/org/apache/archiva/metadata/repository/cassandra/model/ProjectVersionMetadataModel.java
+++ metadata-store-cassandra/src/main/java/org/apache/archiva/metadata/repository/cassandra/model/ProjectVersionMetadataModel.java
@@ -1,18 +1,32 @@
 package org.apache.archiva.metadata.repository.cassandra.model;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import org.apache.archiva.metadata.model.CiManagement;
-import org.apache.archiva.metadata.model.Dependency;
 import org.apache.archiva.metadata.model.IssueManagement;
-import org.apache.archiva.metadata.model.License;
-import org.apache.archiva.metadata.model.MailingList;
 import org.apache.archiva.metadata.model.Organization;
 import org.apache.archiva.metadata.model.Scm;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Olivier Lamy
@@ -27,28 +41,28 @@ public class ProjectVersionMetadataModel
     /**
      * id is the version
      */
-    @Column(name = "id")
+    @Column( name = "id" )
     private String id;
 
-    @Column(name = "url")
+    @Column( name = "url" )
     private String url;
 
-    @Column(name = "name")
+    @Column( name = "name" )
     private String name;
 
-    @Column(name = "description")
+    @Column( name = "description" )
     private String description;
 
-    @Column(name = "organization")
+    @Column( name = "organization" )
     private Organization organization;
 
-    @Column(name = "issueManagement")
+    @Column( name = "issueManagement" )
     private IssueManagement issueManagement;
 
-    @Column(name = "scm")
+    @Column( name = "scm" )
     private Scm scm;
 
-    @Column(name = "issueManagement")
+    @Column( name = "issueManagement" )
     private CiManagement ciManagement;
 
     // FIXME store those values in a separate table
@@ -58,7 +72,7 @@ public class ProjectVersionMetadataModel
 
     //private List<Dependency> dependencies = new ArrayList<Dependency>();
 
-    @Column(name = "incomplete")
+    @Column( name = "incomplete" )
     private boolean incomplete;
 
     public String getRowId()
